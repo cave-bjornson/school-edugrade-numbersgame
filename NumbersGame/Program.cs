4f4e7ad357ab04ec071fcc3b01f1417948150209
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-09-27 22:04:24 +0200

    feat: implement single run
    Game runs w/o looping.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NumbersGame;
+
+Game game = new();
+Console.WriteLine("Welcome! I'm thinking of a number. Can you guess which number? " +
+                  "You get five tries.");
+Console.Write($"Tries left: {game.Tries}. Enter guess: ");
+int guess;
+while (!int.TryParse(Console.ReadLine(), out guess))
+{
+    Console.WriteLine("Enter an integer value: ");
+} 
+game.CheckGuess(guess);
