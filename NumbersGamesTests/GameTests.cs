51fca64bd44d121538c51f79ad065af85ec2ad25
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-09-27 14:43:41 +0200

    implement rest of class
    close #6

--- NumbersGamesTests/GameTests.cs
+++ NumbersGamesTests/GameTests.cs
@@ -5,19 +5,29 @@ namespace NumbersGameTests
 {
     public class GameTests
     {
+        private const int Low = 1;
+        private const int High = 20;
+        private readonly Game _game = new ();
+
+        [Fact]
+        private void CheckRandomNumber_InRange_ReturnTrue()
+        {
+            Assert.InRange(_game.Number, Low, High);
+        }
+
         [Fact]
         public void CheckGuess_SameInput_ReturnTrue()
         {
-            bool result = Game.CheckGuess(0, 0);
+            var result = _game.CheckGuess(_game.Number);
             Assert.True(result);
         }
 
         [Theory]
         [InlineData(-1)]
         [InlineData(1)]
-        public void CheckGuess_LowerHigherInput_ReturnTrue(int value)
+        public void CheckGuess_LowerHigherInput_ReturnFalse(int value)
         {
-            bool result = Game.CheckGuess(value, 0);
+            var result = _game.CheckGuess(_game.Number + value);
             Assert.False(result);
         }
     }
