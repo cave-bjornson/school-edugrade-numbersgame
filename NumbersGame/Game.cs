cc1b0ab69456c9d004c88c1a3af62dd751db006c
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-09-27 14:43:41 +0200

    feat: implement rest of class
    Adds gamestate and constructor. Tests updated.
    
    close #6

--- NumbersGame/Game.cs
+++ NumbersGame/Game.cs
@@ -1,30 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NumbersGameTests")]
-namespace NumbersGame
+
+namespace NumbersGame;
+
+internal class Game
 {
-    internal class Game
+    private const int Low = 1;
+    private const int High = 20;
+    private const int MaxTries = 5;
+    private static readonly Random Rnd = new();
+
+    public Game()
     {
-        /*
-         * Returns true if guess is right. Prints different responses if
-         * guess is right, lower or higher.
-         */
-        public static bool CheckGuess(int guess, int answer)
+        Tries = 0;
+        Number = Rnd.Next(Low, High);
+    }
+
+    public int Number { get; init; }
+    public int Tries { get; private set; }
+
+    /*
+     * Returns true if guess is right. Prints different responses if
+     * guess is right, lower or higher.
+     */
+    public bool CheckGuess(int guess)
+    {
+        Tries++;
+        var diff = guess - Number;
+        var message = diff switch
         {
-            var diff = guess - answer;
-            string message = diff switch
-            {
-                < 0 => "Sorry, you guessed too low!",
-                > 0 => "Sorry, you guessed too high!",
-                _ => "Woho! You made it!"
-            };
-            Console.WriteLine(message);
-            return guess == answer;
+            < 0 => "Sorry, you guessed too low!",
+            > 0 => "Sorry, you guessed too high!",
+            _ => "Wohoo! You made it!"
+        };
+        Console.WriteLine(message);
+        return guess == Number;
+    }
+
+    public bool TriesRemain()
+    {
+        var triesRemain = Tries <= MaxTries;
+        if (!triesRemain)
+        {
+            Console.WriteLine($"Sorry, you didn't guess the right number on {MaxTries} tries");
         }
+
+        return triesRemain;
     }
 }
