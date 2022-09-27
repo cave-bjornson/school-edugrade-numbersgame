02dc7965282769cfdbebb8ba35ea78794f1cf308
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-09-28 00:41:03 +0200

    build: add logging dependencies
    Project uses Microsofts own logging.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,12 +1,19 @@
-﻿using NumbersGame;
+﻿using Microsoft.Extensions.Logging;
+using NumbersGame;
+
+var loggerFactory = LoggerFactory.Create(
+    builder => builder
+        .AddDebug()
+        .SetMinimumLevel(LogLevel.Debug)
+);
+var logger = loggerFactory.CreateLogger<Program>();
+
 
 Game game = new();
-Console.WriteLine("Welcome! I'm thinking of a number. Can you guess which number? " +
-                  "You get five tries.");
+Console.WriteLine("Welcome! I'm thinking of a number. Can you guess which number?");
+Console.WriteLine("You get five tries.");
 Console.Write($"Tries left: {game.Tries}. Enter guess: ");
 int guess;
-while (!int.TryParse(Console.ReadLine(), out guess))
-{
-    Console.WriteLine("Enter an integer value: ");
-} 
+while (!int.TryParse(Console.ReadLine(), out guess)) Console.WriteLine("Enter an integer value: ");
+
 game.CheckGuess(guess);
